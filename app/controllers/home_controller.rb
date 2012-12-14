b69e812c78949c13da93c55bf3fcec9838dc9c51
brookz <brook_realize@126.com>
2012-12-14 20:38:19 +0800

    some locales

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user! #, :only => [:show]
+  
   def index
     @users = User.all
+  end
+  
+  def show
+    
   end
 end
