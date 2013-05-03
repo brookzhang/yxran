e525bbf995179c154b6e90bc2744bb3d64ac91b2
brookzhang <brook_realize@126.com>
2013-05-03 20:28:27 +0800

    index name is too long

--- db/migrate/20121215070114_add_category_product_store_stock_stock_history.rb
+++ db/migrate/20121215070114_add_category_product_store_stock_stock_history.rb
@@ -59,6 +59,6 @@ class AddCategoryProductStoreStockStockHistory < ActiveRecord::Migration
     add_index(:stores, [:name, :category])
     add_index(:stocks, [:product_id, :store_id ])
     add_index(:stock_histories, :stock_id )
-    add_index(:stock_histories, :adjust_type ])
+    add_index(:stock_histories, :adjust_type )
   end
 end
