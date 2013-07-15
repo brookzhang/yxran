c27527ef9ce28ab7d4b8a3958699d81f776eda38
brookz <brook_realize@126.com>
2013-07-15 17:46:52 +0800

    assets compress for development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Yxran::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :log # :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
