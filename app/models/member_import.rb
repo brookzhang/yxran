4722b72d291d14fa109f81c7fa81ec48912ffd80
brookzhang <brook@pathsource.com>
2013-12-29 22:18:36 +0800

    member import

--- app/models/member_import.rb
+++ app/models/member_import.rb
@@ -22,87 +22,51 @@ class MemberImport
     details.default_sheet = details.sheets.first
     
     (2..details.last_row).map do |i|
-      member = Member.find_by_name(details.cell(i,1)) 
-      if member.nil? && details.cell(i,1).size > 0
-        member = Member.new
-        
-        member.name = details.cell(i,1)
-        if details.cell(i,2).is_a?(Integer)
-          member.phone = details.cell(i,2).to_i.to_s
+      if details.cell(i,1).present?
+        member = Member.where(:name => details.cell(i,1), :phone => details.cell(i,2)).first 
+        if member.nil? 
+          member = Member.new(:name => details.cell(i,1), :phone => details.cell(i,2))
         end
-        
-        member.phone = details.cell(i,2).to_i.to_s
-        member.address = details.cell(i,3)
-        member.remark = details.cell(i,4)
-        member.level = details.cell(i,5)
-        member.score = details.cell(i,6)
-        member.all_score = member.score > 0 ? member.score : 0
+        member.address = details.cell(i,3) if details.cell(i,3).present?
+        member.remark = details.cell(i,4) if details.cell(i,4).present?
+        member.level = details.cell(i,5) if details.cell(i,5).present?
+        member.level ||= 0 
+        member.score = details.cell(i,6) if details.cell(i,6).present?
+        member.score ||= 0 
+        member.start_date = details.cell(i,7) if details.cell(i,7).present?
+        member.all_score = member.score 
         member.user_id = user_id
         member.uuid = UUIDTools::UUID.random_create().to_s
-        
-        member.save!
+        member.save
       end
       
     end
     
-    if errors.any?
-      false
-    else
-      true
-    end
-    
-    
+    errors.empty?
   end
+
+
+
   
   def valid?
     details = open_spreadsheet
     details.default_sheet = details.sheets.first
     
     (2..details.last_row).map do |i|
-      (1..6).map do |col|
-        if details.cell(i,col).nil? || details.cell(i,col) == '' 
-          errors.add(:base, :wrong_format)
-        end
-      end
-        
-      if details.cell(i,1).nil? || details.cell(i,1).size == 0
-        errors.add(:base, "#{i}:A  wrong name.")
-      end
-        
-      if details.cell(i,2).nil? || details.cell(i,2).size == 0
-        errors.add(:base, "#{i}:B  wrong phone.")
-      end
-      
-      
-      
-      if !details.cell(i,5).to_i.is_a?(Integer)
-        errors.add(:base, "#{i}:E  wrong level.")
-      end
-      if !details.cell(i,6).to_i.is_a?(Integer)
-        errors.add(:base, "#{i}:F  wrong score.")
-      end
-        
-      member = Member.find_by_name(details.cell(i,1)) 
-      if member.nil?
-        member = Member.new
-        lookup = Lookup.get_one("member", details.cell(i,5).to_i.to_s)
-        if lookup.nil? 
-          errors.add(:base, "#{i}:E  level not exists#{details.cell(i,5)}.")
-        end
-        
-        #errors.add(:base, "#{i}:E  level not exists#{details.cell(i,5)},uuid=#{UUIDTools::UUID.random_create().to_s}.")
-        
-        
-      end 
-      
-      
+      errors.add(:base, I18n.t(:please_input_name_on_line_n, :line => i)) if details.cell(i,1).blank?
+      errors.add(:base, I18n.t(:please_input_phone_on_line_n, :line => i)) if details.cell(i,2).blank?
+      errors.add(:base, I18n.t(:please_input_right_level_on_line_n, :line => i)) if details.cell(i,5).present? && ![0,1,9].include?(details.cell(i,5))
+      errors.add(:base, I18n.t(:please_input_score_on_line_n, :line => i)) if details.cell(i,6).blank?
+      errors.add(:base, I18n.t(:please_input_right_score_on_line_n, :line => i)) if details.cell(i,6).present? && !details.cell(i,6).is_a?(Float)
+      errors.add(:base, I18n.t(:please_input_right_date_on_line_n, :line => i)) if details.cell(i,7).present? && !details.cell(i,7).is_a?(Date)
+      puts "==========="
+      puts details.cell(i,7)
+      puts "==#{(Integer.parse(details.cell(i,6)) rescue nil).nil?}==" 
+      puts "==#{ details.cell(i,6).class}==" 
+      puts "==========="
     end
-    
-    if errors.any?
-      false
-    else
-      true
-    end
+
+    errors.empty?
   end
   
 
